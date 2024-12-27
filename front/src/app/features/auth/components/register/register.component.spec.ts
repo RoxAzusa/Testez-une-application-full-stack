7b14eab733967bf4ede0941d9fad3a5e511a7a1b
Roxane Crépeaux <crepeaux.roxane@gmail.com>
2024-12-27 16:00:16 +0100

    Create unit test for register component

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -9,10 +9,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
 
 import { RegisterComponent } from './register.component';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let authService: AuthService;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -28,7 +33,9 @@ describe('RegisterComponent', () => {
       ]
     })
       .compileComponents();
-
+    
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -36,5 +43,67 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('register', () => {
+    const registerComponent = fixture.componentInstance;
+    registerComponent.form.controls['email'].setValue('test@test.com');
+    registerComponent.form.controls['firstName'].setValue('Anna');
+    registerComponent.form.controls['lastName'].setValue('Field');
+    registerComponent.form.controls['password'].setValue('Anna123');
+
+    expect(registerComponent.form.valid).toBeTruthy();
+  });
+
+  it('failed register without email', () => {
+    const registerComponent = fixture.componentInstance;
+    registerComponent.form.controls['firstName'].setValue('Anna');
+    registerComponent.form.controls['lastName'].setValue('Field');
+    registerComponent.form.controls['password'].setValue('Anna123');
+
+    expect(registerComponent.form.valid).toBeFalsy();
+  });
+
+  it('failed register without first name', () => {
+    const registerComponent = fixture.componentInstance;
+    registerComponent.form.controls['email'].setValue('test@test.com');
+    registerComponent.form.controls['lastName'].setValue('Field');
+    registerComponent.form.controls['password'].setValue('Anna123');
+
+    expect(registerComponent.form.valid).toBeFalsy();
+  });
+
+  it('failed register without last name', () => {
+    const registerComponent = fixture.componentInstance;
+    registerComponent.form.controls['email'].setValue('test@test.com');
+    registerComponent.form.controls['firstName'].setValue('Anna');
+    registerComponent.form.controls['password'].setValue('Anna123');
+
+    expect(registerComponent.form.valid).toBeFalsy();
+  });
+
+  it('failed register without password', () => {
+    const registerComponent = fixture.componentInstance;
+    registerComponent.form.controls['email'].setValue('test@test.com');
+    registerComponent.form.controls['firstName'].setValue('Anna');
+    registerComponent.form.controls['lastName'].setValue('Field');
+
+    expect(registerComponent.form.valid).toBeFalsy();
+  });
+
+  it('register with response', () => {
+    const registerComponent = fixture.componentInstance;
+    registerComponent.form.controls['email'].setValue('test@test.com');
+    registerComponent.form.controls['firstName'].setValue('Anna');
+    registerComponent.form.controls['lastName'].setValue('Field');
+    registerComponent.form.controls['password'].setValue('Anna123');
+
+    let mockAuthService = jest.spyOn(authService, 'register').mockImplementation(() => of(undefined));
+    let mockRouter = jest.spyOn(router, 'navigate').mockImplementation(async() => true);
+
+    registerComponent.submit();
+
+    expect(mockAuthService).toHaveBeenCalled();
+    expect(mockRouter).toHaveBeenLastCalledWith(['/login']);
   });
 });

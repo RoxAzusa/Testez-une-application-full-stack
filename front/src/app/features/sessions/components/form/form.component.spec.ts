5786ea940bc86f94cb77e35d7eccf894fd7c70c5
Roxane Crépeaux <crepeaux.roxane@gmail.com>
2025-01-18 14:57:58 +0100

    Create unit test for form component

--- front/src/app/features/sessions/components/form/form.component.spec.ts
+++ front/src/app/features/sessions/components/form/form.component.spec.ts
@@ -14,10 +14,15 @@ import { SessionService } from 'src/app/services/session.service';
 import { SessionApiService } from '../../services/session-api.service';
 
 import { FormComponent } from './form.component';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
+  let sessionApiService: SessionApiService;
+  let router: Router;
+  let sessionService: SessionService;
 
   const mockSessionService = {
     sessionInformation: {
@@ -48,6 +53,9 @@ describe('FormComponent', () => {
     })
       .compileComponents();
 
+    sessionApiService = TestBed.inject(SessionApiService);
+    router = TestBed.inject(Router);
+    sessionService = TestBed.inject(SessionService);
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -56,4 +64,57 @@ describe('FormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize form with empty values when creating a new session', () => {
+    const registerComponent = fixture.componentInstance;
+    expect(registerComponent.sessionForm?.value).toEqual({
+      name: '',
+      date: '',
+      teacher_id: '',
+      description: ''
+    });
+  });
+  
+  it('should have form invalid when required fields are empty', () => {
+    const registerComponent = fixture.componentInstance;
+    registerComponent.sessionForm?.setValue({
+      name: '',
+      date: '',
+      teacher_id: '',
+      description: ''
+    });
+    expect(registerComponent.sessionForm?.valid).toBeFalsy();
+  });
+
+  it('should initialize form with default empty values when no session is provided', () => {
+    const registerComponent = fixture.componentInstance;
+    registerComponent['initForm']();
+    expect(registerComponent.sessionForm?.value).toEqual({
+      name: '',
+      date: '',
+      teacher_id: '',
+      description: ''
+    });
+  });
+  
+  it('should initialize form with session values when a session is provided', () => {
+    const registerComponent = fixture.componentInstance;
+    const session = {
+      id: 1,
+      name: 'yoga',
+      description: 'yoga',
+      date: new Date,
+      teacher_id: 1,
+      users: [1],
+      createdAt: new Date,
+      updatedAt: new Date
+    }
+    registerComponent['initForm'](session);
+    expect(registerComponent.sessionForm?.value).toEqual({
+      name: session.name,
+      date: '2025-01-18',
+      teacher_id: session.teacher_id,
+      description: session.description
+    });
+  });  
 });
